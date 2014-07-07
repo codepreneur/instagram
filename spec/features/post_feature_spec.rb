573e7377d2c1c70444c5665a9f6184f58be99102
gomenasaai <contramundee@gmail.com>
2014-07-07 16:45:40 +0100

    preparing to create a new branch

--- spec/features/post_feature_spec.rb
+++ spec/features/post_feature_spec.rb
@@ -28,29 +28,32 @@ end
 
 describe "creating posts" do
 
-	it "can add a post" do
-		visit '/posts/new'
-		fill_in 'Title', with: 'Cool post'
-		fill_in 'Description', with: 'This is cool description'
-		click_button "Post it!"
-		expect(current_path).to eq '/posts'
-		expect(page).to have_content 'Cool post'
-		expect(page).to have_content 'This is cool description'
-		expect(page).not_to have_css 'img.uploaded-pic'
-	end	
 
-	it "can upload the photo" do
-		visit '/posts/new'
-		fill_in 'Title', with: 'Cool post'
-		fill_in 'Description', with: 'This is cool description'
-		attach_file 'Image', Rails.root.join('spec/images/cat.jpg')
+		it "can add a post" do
+			visit '/posts/new'
+			fill_in 'Title', with: 'Cool post'
+			fill_in 'Description', with: 'This is cool description'
+			click_button "Post it!"
+			expect(current_path).to eq '/posts'
+			expect(page).to have_content 'Cool post'
+			expect(page).to have_content 'This is cool description'
+			expect(page).not_to have_css 'img.uploaded-pic'
+		end	
 
-		click_button "Post it!"
-		expect(current_path).to eq posts_path
-		expect(page).to have_content 'Cool post'
-		expect(page).to have_content 'This is cool description'
-		expect(page).to have_css 'img.uploaded-pic'
-	end	
+		it "can upload the photo" do
+			visit '/posts/new'
+			fill_in 'Title', with: 'Cool post'
+			fill_in 'Description', with: 'This is cool description'
+			attach_file 'Image', Rails.root.join('spec/images/cat.jpg')
+
+			click_button "Post it!"
+			expect(current_path).to eq posts_path
+			expect(page).to have_content 'Cool post'
+			expect(page).to have_content 'This is cool description'
+			expect(page).to have_css 'img.uploaded-pic'
+		end	
+
+
 
 end
 
