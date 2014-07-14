7661eac7554eba8955d4bbde012c1f818e46bc46
gomenasaai <contramundee@gmail.com>
2014-07-14 18:59:33 +0100

    can display orders now

--- config/routes.rb
+++ config/routes.rb
@@ -7,9 +7,11 @@ Rails.application.routes.draw do
 
   resources :posts do
     resource :map
+    resources :charges
   end
 
   resources :tags
+  resources :orders
   
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
